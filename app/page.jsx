afba4d4bd01fe2aabe2d0cba6036a4e49f182210
areccmith204-png <areccmith204@gmail.com>
2026-01-21 15:54:42 +0500

    Add Home component with basic layout

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// app/page.jsx
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>TALANT TEAM</h1>
+      <p>Sayt ishlayapti ✅</p>
+    </div>
+  );
+}
