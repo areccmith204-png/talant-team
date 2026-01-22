d521eb6286f3f4aa7a94ae4637179cb0798f69b3
areccmith204-png <areccmith204@gmail.com>
2026-01-22 13:05:19 +0500

    Update Firebase configuration and import analytics

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,15 +1,21 @@
-// firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEYING",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDFkInTgrZW1eswniPpXG3XsG_gWVJOuYQ",
+  authDomain: "talant-team.firebaseapp.com",
+  projectId: "talant-team",
+  storageBucket: "talant-team.firebasestorage.app",
+  messagingSenderId: "359675211022",
+  appId: "1:359675211022:web:5e77a8405af95ae7843b5f",
+  measurementId: "G-W8RREZNSL4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
