473ddb674561acaab3c6722faf89e82024083029
areccmith204-png <areccmith204@gmail.com>
2026-01-22 12:49:37 +0500

    Refactor Firebase config for Firestore usage
    Updated Firebase configuration to use Firestore and replaced sensitive information with placeholders.

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFkInTgrZW1eswniPpXG3XsG_gWVJOuYQ",
-  authDomain: "talant-team.firebaseapp.com",
-  projectId: "talant-team",
-  storageBucket: "talant-team.firebasestorage.app",
-  messagingSenderId: "359675211022",
-  appId: "1:359675211022:web:5e77a8405af95ae7843b5f",
-  measurementId: "G-W8RREZNSL4"
+  apiKey: "API_KEYING",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
